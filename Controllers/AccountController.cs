487df359507e2dfa3d4c57d7756c5b65ca227e84
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2019-06-14 13:48:45 -0300

    login_method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NG_Core_Auth.Helpers;
 using NG_Core_Auth.Models;
+using System.Linq;
+using System.Text;
 
 namespace NG_Core_Auth.Controllers
 {
@@ -15,13 +21,16 @@ namespace NG_Core_Auth.Controllers
 
         private readonly SignInManager<IdentityUser> _signManager;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        private readonly AppSettings _appSettings;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager, IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
             _signManager = signManager;
+            _appSettings = appSettings.Value;
         }
 
-        [HttpPost("action")]
+        [HttpPost("[action]")]
         public async Task<IActionResult> Register([FromBody]RegisterViewModel formdata)
         {
             // Will hold all the errors related to the registration
@@ -53,6 +62,51 @@ namespace NG_Core_Auth.Controllers
             }
 
             return BadRequest(new JsonResult(errorList);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody]LoginViewModel formdata)
+        {
+            var user = await _userManager.FindByNameAsync(formdata.Username);
+
+            var roles =  await _userManager.GetRolesAsync(user);
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+
+            double tokenExpireTime = Convert.ToDouble(_appSettings.ExpireTime);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, formdata.Password))
+            {
+                // generate token
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, formdata.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                        new Claim("LoggedOn", DateTime.Now.ToString())
+                    }),
+
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+
+                    Issuer = _appSettings.Site,
+
+                    Audience = _appSettings.Audience,
+
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpireTime)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), expiration = token.ValidTo, username = user.UserName, userRole = roles.FirstOrDefault() });
+            }
+
+            ModelState.AddModelError("", "Username/Password was not found");
+            return Unauthorized(new { LoginError = "Please check the login credentials - Invalid Username/Password was entered" });
         }
     }
 }
